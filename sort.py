0736c240e4061b33ba97e229c889d540f58a76db
Lucas Siu <ls1020@ic.ac.uk>
2025-03-03 14:42:10 +0000

    added comments

--- sort.py
+++ sort.py
@@ -1,10 +1,24 @@
 def pivot_sort(arr):
+    """
+    Sorts an array using the pivot sort (similar to quicksort) algorithm.
+
+    Args:
+        arr (list): The list of elements to be sorted.
+
+    Returns:
+        list: A new list with the elements from arr sorted in ascending order.
+    """
     if len(arr) <= 1:
+        # Base case: an array of zero or one elements is already sorted
         return arr
     else:
+        # Choose the first element as the pivot
         pivot = arr[0]
+        # Elements less than or equal to the pivot
         less_than_pivot = [x for x in arr[1:] if x <= pivot]
+        # Elements greater than the pivot
         greater_than_pivot = [x for x in arr[1:] if x > pivot]
+        # Recursively sort the sublists and concatenate them with the pivot
         return pivot_sort(less_than_pivot) + [pivot] + pivot_sort(greater_than_pivot)
 
 # Example usage
