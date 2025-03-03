42fd3e63da6537e740373e40c9df8d6d0fda6ebf
Lucas Siu <ls1020@ic.ac.uk>
2025-03-03 14:55:05 +0000

    Revert "added example"
    This reverts commit 42e9ba2c0e621295d5f3a34540593c2d92b3279a.

--- sort.py
+++ sort.py
@@ -11,12 +11,13 @@ def pivot_sort(arr):
     if len(arr) <= 1:
         # Base case: an array of zero or one elements is already sorted
         return arr
-    else:
-        # Choose the first element as the pivot
-        pivot = arr[0]
-        # Elements less than or equal to the pivot
-        less_than_pivot = [x for x in arr[1:] if x <= pivot]
-        # Elements greater than the pivot
-        greater_than_pivot = [x for x in arr[1:] if x > pivot]
-        # Recursively sort the sublists and concatenate them with the pivot
-        return pivot_sort(less_than_pivot) + [pivot] + pivot_sort(greater_than_pivot)
+    pivot = arr[0]
+
+    # Partition the array into two sub-arrays:
+    # 'left' with elements less than the pivot
+    # 'right' with elements greater than or equal to the pivot
+    left = [x for x in arr[1:] if x < pivot]
+    right = [x for x in arr[1:] if x >= pivot]
+
+    # Recursively sort the sub-arrays and combine them with the pivot
+    return pivot_sort(left) + [pivot] + pivot_sort(right)
